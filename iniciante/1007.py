9200ebb2d828c1aac1b197a21a469aee05ab34bd
Benjamin Santos Silva <benjamin.silva.bs@ufpi.edu.br>
2025-01-02 08:22:10 -0300

    Solution to problem 1007

---
+++ iniciante/1007.py
@@ -0,0 +1,13 @@
+def main():
+    A = int(input())
+    B = int(input())
+    C = int(input())
+    D = int(input())
+
+    difference = (A * B - C * D)
+
+    print('DIFERENCA = {}'.format(difference))
+
+
+if __name__ == '__main__':
+    main()
