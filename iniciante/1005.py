723397b44c9b67792d41e7c2dec79e600f75525d
Benjamin Santos Silva <benjamin.silva.bs@ufpi.edu.br>
2025-01-02 08:13:49 -0300

    Solution to problem 1005

---
+++ iniciante/1005.py
@@ -0,0 +1,11 @@
+def main():
+    A = float(input())
+    B = float(input())
+
+    average = (A * 3.5 + B * 7.5) / 11
+
+    print('MEDIA = {:.5f}'.format(average))
+
+
+if __name__ == '__main__':
+    main()
